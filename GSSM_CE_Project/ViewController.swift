ad6728ed381fdc335dfd3a7246c24d698b99adca
jbingham15 <jbingham15@m0004.local>
2015-03-19 10:54:10 -0400

    Current Card is a dead end, scraping to make it better

--- GSSM_CE_Project/ViewController.swift
+++ GSSM_CE_Project/ViewController.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    @IBOutlet weak var textField6: UITextField!
+    @IBOutlet weak var textField7: UITextField!
+    @IBOutlet weak var textField8: UITextField!
+    @IBOutlet weak var textField9: UITextField!
+    @IBOutlet weak var textField10: UITextField!
+    @IBOutlet weak var textField11: UITextField!
+    @IBOutlet weak var textField12: UITextField!
+    @IBOutlet weak var textField13: UITextField!
+    @IBOutlet weak var textField14: UITextField!
+    @IBOutlet weak var textField15: UITextField!
+    @IBOutlet weak var textField16: UITextField!
+    @IBOutlet weak var textField17: UITextField!
+    @IBOutlet weak var textField18: UITextField!
+    @IBOutlet weak var textField19: UITextField!
+    @IBOutlet weak var textField20: UITextField!
+    @IBOutlet weak var textField21: UITextField!
+    @IBOutlet weak var textField22: UITextField!
+    @IBOutlet weak var textField23: UITextField!
+    @IBOutlet weak var textField24: UITextField!
+    @IBOutlet weak var textField25: UITextField!
+    @IBOutlet weak var textField26: UITextField!
+    @IBOutlet weak var textField27: UITextField!
+    @IBOutlet weak var textField28: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +47,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
